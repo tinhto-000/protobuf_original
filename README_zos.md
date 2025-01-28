cf8f48ca2c8bc94b2fe84456c39b7d60dfa69f9d
Tin H To <tinto@us.ibm.com>
2025-01-28 10:12:51 -0500

    updated z/OS changes

--- README_zos.md
+++ README_zos.md
@@ -1,16 +1,50 @@
-How to compile
-===================================================
+# How to compile
 
-Install `cmake`, `zoslib` and `zlib` from z/OS Open Tools:
+The following packages have been modified for z/OS:
 
-https://zosopentools.github.io/meta/#/Guides/QuickStart
+1. protobuf (this repo)
+2. abseil-cpp
+3. googletest
 
-Make sure `ibm-clang64` and `ibm-clang++64` are on the system.
+This document assumes you have access to 2 different zopen installations:
 
-Then:
+- Shared (e.g., `/usr/local/zopen/`) (i.e., some place that you do not have
+  permisssion to install packages to)
+  - Enviroment variable `ZOPEN_ROOTFS` points to this
+  - Already contains standard/popular packages such as `git`, `make`, `cmake`,
+    `zoslib`
+- Local (e.g., `$HOME/zopen/`) (some place that you have complete control of)
+  - Enviroment variable `MY_ZOPEN_ROOTFS` points to this
+  - You might need this so that you can install `zlib` and `cmake` version
+    **3.24.x**
+  - Instruction: <https://zosopentools.github.io/meta/#/Guides/QuickStart>
+  - You **MUST** use`cmake` v3.24.x, otherwise you'd get errors like this during
+    link:
 
 ```
-ZOPEN_INSTALL_DIR=/xxx/yyy/zzz/zopen/prod zos_build.sh
+ar: FSUM9942 "CMakeFiles/libprotoc.dir/src/google/protobuf/compiler/python/helpers.cc.o" ignored, same basename as "CMakeFiles/libprotoc.dir/src/google/protobuf/compiler/cpp/helpers.cc.o".
 ```
+
+Also the z/OS system must have "Open XL C/C++ for z/OS" (`ibm-clang64`,
+`ibm-clang++64`) installed by the system programmer.
+
+# Steps
+
+1. `git clone --recursive` clone this repo and this branch. This command will
+   also pull down the third-party dependencies.
+2. Replace `abseil-cpp` and `googletest` in `/third_party` with:
+
+```
+git clone git@github.com:tinhto-000/abseil-cpp.git -b 20230125.3_zos
+git clone git@github.com:tinhto-000/googletest.git -b v1.12.1_zos
+```
+
+3. source `zopen-config` from the shared zopen location (e.g.
+   `source /usr/local/zopen/etc/zopen-config`) so that `ZOPEN_ROOTFS` is set.
+4. `export MY_ZOPEN_ROOTFS` to where **YOUR** zopen installation is (so that it
+   can find `zlib`)
+5. `export PATH` to where `cmake` **v3.24.x** `bin` dir is (e.g.,
+   `export PATH=$HOME/CMake-heads.v3.24.2/bin:$PATH`)
+6. issue `zos_build.sh`
 
 `protoc` and static libraries are generated at `./build`
--- zos_build.sh
+++ zos_build.sh
@@ -1,46 +1,50 @@
 #!/usr/bin/env bash
 
-if [[ -z "$ZOPEN_INSTALL_DIR" ]]; then
-    echo "Must have env var ZOPEN_INSTALL_DIR" 1>&2
-    echo "export ZOPEN_INSTALL_DIR=/xxx/yyy/zzz/zopen/prod" 1>&2
+if [[ -z "$ZOPEN_ROOTFS" ]]; then
+    echo "Please source /usr/local/zopen/etc/zopen-config first" 1>&2
+    exit 1
+fi
+
+if [[ -z "$MY_ZOPEN_ROOTFS" ]]; then
+    echo "Please set MY_ZOPEN_ROOTFS" 1>&2
+    echo "e.g., export MY_ZOPEN_ROOTFS=$HOME/zopen" 1>&2
     exit 1
 fi
 
 export CC="ibm-clang64"
 export CXX="ibm-clang++64"
 
-# standard CFLAGS/CXXFLAGS used in Python SDK and Python AI Toolkit
-
-export CFLAGS=\
-"-DNDEBUG -march=arch10 -O3 -std=c11 -m64 -D_XOPEN_SOURCE=600 "\
-"-D_UNIX03_THREADS -D_POSIX_THREADS -D_OPEN_SYS_FILE_EXT -m64 -fasm "\
-"-fvisibility=default -fzos-le-char-mode=ascii -fno-short-enums "\
+# similar CFLAGS/CXXFLAGS used in Python SDK and Python AI Toolkit
+# ***** ibm-clang crahses compiling generated_message_tctable_lite.cc with -O3
 
 export CXXFLAGS=\
-"-DNDEBUG -march=arch10 -O3 -m64 -D_XOPEN_SOURCE=600 "\
+"-DNDEBUG -march=arch10 -O2 -m64 -D_XOPEN_SOURCE=600 "\
 "-D_UNIX03_THREADS -D_POSIX_THREADS -D_OPEN_SYS_FILE_EXT -m64 -fasm "\
 "-fvisibility=default -fzos-le-char-mode=ascii -fno-short-enums "\
+
+export CFLAGS="-std=c11 "$CXXFLAGS
 
 # ZOSLIB_OVERRIDE_CLIB=1 to enable zoslib's std functions override
 # (for some reasons target=zosv2r5 fails CMAKE_HAVE_LIBC_PTHREAD check)
 export EXTRA_FLAGS=" -DZOSLIB_OVERRIDE_CLIB=1 -mzos-target=zosv2r4 \
--I$ZOPEN_INSTALL_DIR/zoslib/include "
+-I$ZOPEN_ROOTFS/usr/local/include "
 
 export CFLAGS=$CFLAGS" "$EXTRA_FLAGS
 export CXXFLAGS=$CXXFLAGS" "$EXTRA_FLAGS
 
 export LDFLAGS="\
-$ZOPEN_INSTALL_DIR/zoslib/lib/libzoslib.a \
-$ZOPEN_INSTALL_DIR/zoslib/lib/libzoslib-supp.a"
+$ZOPEN_ROOTFS/usr/local/lib/libzoslib.a \
+$ZOPEN_ROOTFS/usr/local/lib/libzoslib-supp.a"
 
 rm -fr build
 mkdir build
 cd build
 
-cmake ../cmake -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
+# ***** ibm-clang crahses compiling generated_message_tctable_lite.cc with -O3
+# ***** "-DCMAKE_BUILD_TYPE=Release" adds "-O3 -DNDEBUG", so can't use that either
+cmake .. -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CXXFLAGS" \
 -Dprotobuf_BUILD_SHARED_LIBS=OFF -Dprotobuf_BUILD_TESTS=OFF \
--DZLIB_INCLUDE_DIR="$ZOPEN_INSTALL_DIR/zlib/include" \
--DZLIB_LIBRARY="$ZOPEN_INSTALL_DIR/zlib/lib/libz.a" \
--DCMAKE_BUILD_TYPE=Release
+-DZLIB_INCLUDE_DIR="$MY_ZOPEN_ROOTFS/usr/local/include" \
+-DZLIB_LIBRARY="$MY_ZOPEN_ROOTFS/usr/local/lib/libz.a"
 
 cmake --build .
